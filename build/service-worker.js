5e0d90be78ee32016cf4b8b0aa0a2c2162e9fffc
Alex Lindsay <alindsaydev@gmail.com>
2020-04-03 21:16:08 -0700

    build for Beta release

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f35ec555ca5985daca52dc7d88b68ca.js"
+  "/precache-manifest.16c887b6050775603ae653bf408ccf89.js"
 );
 
 workbox.clientsClaim();
