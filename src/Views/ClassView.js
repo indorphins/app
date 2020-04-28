bbfbf2b5b0f5a6882616035c98a4438548e71e7c
Alex Lindsay <alindsaydev@gmail.com>
2020-04-27 23:11:15 -0700

    use env var in room init

--- src/Views/ClassView.js
+++ src/Views/ClassView.js
@@ -77,7 +77,7 @@ const ClassView = (props) => {
 						exp:
 							Math.floor(Date.now() / 1000) +
 							parseInt(process.env.REACT_APP_DEFAULT_CLASS_DURATION),
-						max_participants: 21, // 10 participants + 1 instructor
+						max_participants: process.env.REACT_APP_DEFAULT_CLASS_SIZE,
 						eject_at_room_exp: true,
 					},
 				})
