f5441d6fb35ff48f1e7d2afe6b9d062e20a40cb3
afloesch <andrew@vapi.com>
2020-07-30 13:06:30 -0700

    move scrollToTop to utils

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux'
 
-import ScrollToTop from './components/scrollToTop';
+import ScrollToTop from './utils/scrollToTop';
 import { store } from './store';
 
 import App from './app';
