9650663d95dc61866e797462bba545d7f977a69f
afloesch <andrew@vapi.com>
2020-08-06 15:13:25 -0700

    modify DOB picker to split selection into Y/M/D

--- src/components/birthday.js
+++ src/components/birthday.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 export const Birthday = (props) => {
 
+  const [ date, setDate ] = useState(null);
+
+  function handleChange(evt) {
+    setDate(evt);
+    if (props.onChange) {
+      props.onChange(evt);
+    }
+  }
+
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils} >
-      <KeyboardDatePicker
-        className={props.classStyle}
-        disabled={props.loader}
-        required
-        variant="inline"
-        format="MM/dd/yyyy"
-        placeholder="mm/dd/yyyy"
-        margin="normal"
-        id="birthday-input"
-        label="Birthday"
-        autoComplete='bday'
-        value={props.val}
-        onFocus={props.focus}
-        onChange={props.change}
-        helperText={props.err}
-        KeyboardButtonProps={{
-          'aria-label': 'change date',
-        }}
-      />
+      <Grid container direction="row" spacing={2}>
+        <Grid item>
+          <KeyboardDatePicker
+            className={props.classStyle}
+            disabled={props.loader}
+            required
+            views={["year"]}
+            variant="inline"
+            format="yyyy"
+            label="Year"
+            value={date}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item>
+        <KeyboardDatePicker
+            className={props.classStyle}
+            disabled={props.loader}
+            required
+            views={["month"]}
+            variant="inline"
+            format="MMMM"
+            label="Month"
+            value={date}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item>
+        <KeyboardDatePicker
+            className={props.classStyle}
+            disabled={props.loader}
+            required
+            //views={["day"]}
+            variant="inline"
+            format="d"
+            label="Day"
+            value={date}
+            onChange={handleChange}
+          />
+        </Grid>
+      </Grid>
       </MuiPickersUtilsProvider>
   )
 };
