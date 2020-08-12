c0fcbaef1b2682b52213c95564bb459b70b820cf
Alex Lindsay <alindsaydev@gmail.com>
2020-08-11 19:13:21 -0700

    birthday window on both sides of class date

--- src/components/otherCourseInfo.js
+++ src/components/otherCourseInfo.js
@@ -12,10 +12,10 @@ export const OtherCourseInfo = (props) => {
 
   return (
     <Grid container direction='column' className={classes.content}>
-        <Typography variant="body2">Other things to know:</Typography>
-        <Typography variant="body2"> - You'll join the class right here</Typography>
-        <Typography variant="body2"> - Join using a laptop/computer</Typography>
-        <Typography variant="body2"> - You can cancel up to 24 hours before the class starts</Typography>
+      <Typography variant="body2">Other things to know:</Typography>
+      <Typography variant="body2"> - You'll join the class right here</Typography>
+      <Typography variant="body2"> - Join using a laptop/computer</Typography>
+      <Typography variant="body2"> - You can cancel up to 24 hours before the class starts</Typography>
     </Grid>
   )
 }
