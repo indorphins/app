1775f7b599a1902256158859b7270b8def83c54d
afloesch <andrew@vapi.com>
2020-08-12 18:22:01 -0700

    move participants to its own component

--- src/components/courseInfo/index.js
+++ src/components/courseInfo/index.js
@@ -2,10 +2,12 @@ import Cost from './cost';
 import AvailableSpots from './availableSpots';
 import Duration from './duration';
 import StartTime from './startTime';
+import Participants from './participants';
 
 export {
   Cost,
   Duration,
   AvailableSpots,
-  StartTime
+  StartTime,
+  Participants
 };
