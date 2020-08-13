2cc67520e9a9a810788ba26e054afbce2e83d0c6
afloesch <andrew@vapi.com>
2020-08-12 19:21:04 -0700

    factor our user data lookup / creation

--- src/app.js
+++ src/app.js
@@ -23,17 +23,7 @@ const stripePromise = loadStripe(config.stripe_public_key);
 export default function App() {
   const currentUser = useSelector((state) => getUserSelector(state));
 
-  async function listener(firebaseUserData) {
-    // user logged out or session expired
-    if (!firebaseUserData) {
-      log.debug('AUTH:: firebase user null');
-      if (currentUser.id) {
-        await store.dispatch(actions.user.clear());
-      }
-      return;
-    }
-
-    log.debug('AUTH:: got firebase user data', firebaseUserData);
+  async function getUser(firebaseUserData) {
     let user;
 
     // try to fetch indorphins user data
@@ -71,35 +61,46 @@ export default function App() {
             phone,
           );
         } catch (err) {
-          return log.warn(
+          log.warn(
             'AUTH:: error creating user account from firebase token',
             err
           );
+
+          return user;
         }
       } else {
-        return log.error('AUTH:: Call User.get', err);
+        throw err;
       }
     }
 
-    if (user && user.data) {
-      log.debug('AUTH:: got indorphins user data', user.data);
+    return user;
+  }
 
-      try {
-        return await store.dispatch(actions.user.set(user.data));
-      } catch (err) {
-       
-        return log.error('AUTH:: save user to store', err);
+  async function listener(firebaseUserData) {
+    // user logged out or session expired
+    if (!firebaseUserData) {
+      log.debug('AUTH:: firebase user null');
+      if (currentUser.id) {
+        await store.dispatch(actions.user.clear());
       }
+      return;
+    }
+
+    log.debug('AUTH:: got firebase user data', firebaseUserData);
+    let user;
+
+    // try to fetch indorphins user data
+    try {
+      user = await getUser(firebaseUserData);
+    } catch (err) {
+      return log.error('AUTH:: get user data', err);
     }
 
     if (!user.data) {
       return log.warn('AUTH:: user creation failed');
     }
-
-    log.debug(
-      'AUTH:: user automatically created from firebase login',
-      user.data
-    );
+    
+    log.debug('AUTH:: got indorphins user data', user.data);
 
     try {
       await store.dispatch(actions.user.set(user.data));
