2af82a6a0f7be7e0fb7573169269be9328eec240
afloesch <andrew@vapi.com>
2020-08-12 19:33:31 -0700

    move other course info component

--- src/components/courseInfo/index.js
+++ src/components/courseInfo/index.js
@@ -4,12 +4,14 @@ import Duration from './duration';
 import StartTime from './startTime';
 import Participants from './participants';
 import JoinSession from './joinSession';
+import OtherCourseInfo from './otherCourseInfo';
 
 export {
   AvailableSpots,
   Cost,
   Duration,
   JoinSession,
+  OtherCourseInfo,
   StartTime,
   Participants
 };
