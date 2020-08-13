cee6e5096b80ee9b38b39ec08bdfac2f045a82b2
afloesch <andrew@vapi.com>
2020-08-12 18:06:43 -0700

    more complexity refactoring

--- src/components/courseInfo/index.js
+++ src/components/courseInfo/index.js
@@ -1,9 +1,11 @@
 import Cost from './cost';
 import AvailableSpots from './availableSpots';
 import Duration from './duration';
+import StartTime from './startTime';
 
 export {
   Cost,
   Duration,
-  AvailableSpots
+  AvailableSpots,
+  StartTime
 };
