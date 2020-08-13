1fcf9853a6f71a732a44b1544c984a026e98ff16
afloesch <andrew@vapi.com>
2020-08-12 18:22:01 -0700

    move participants to its own component

--- src/pages/course/info.js
+++ src/pages/course/info.js
@@ -12,11 +12,10 @@ import {
   Modal, 
   Fade 
 } from '@material-ui/core';
-import { Photo, People, RecordVoiceOver } from '@material-ui/icons';
+import { Photo, RecordVoiceOver } from '@material-ui/icons';
 import Alert from '@material-ui/lab/Alert';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
-import { isSameDay, isWithinInterval, sub, add } from 'date-fns';
 
 import { store, actions } from '../../store';
 import CreateMessage from '../../components/form/createMessage'
@@ -24,8 +23,7 @@ import * as Course from '../../api/course';
 import * as Stripe from '../../api/stripe';
 import log from '../../log';
 import path from '../../routes/path';
-import { BdayIcon } from '../../components/icon/bday';
-import { AvailableSpots, Cost, Duration, StartTime } from '../../components/courseInfo/index';
+import { AvailableSpots, Cost, Duration, StartTime, Participants } from '../../components/courseInfo/index';
 import CoursePayment from '../../components/form/coursePayment';
 
 import { getNextSession } from '../../utils';
@@ -165,7 +163,7 @@ const selectDefaultPaymentMethod = createSelector(
   pd => pd.methods.filter(item => item.default)
 );
 
-export default function () {
+export default function CourseInfo() {
 
   const classes = useStyles();
   const history = useHistory();
@@ -185,7 +183,6 @@ export default function () {
   const [cancel, setCancel] = useState(null);
   const [cancellingClass, setCancellingClass] = useState(false);
   const [confirmCancel, setConfirmCancel] = useState(false);
-  const [participantList, setParticipantList] = useState(null);
 
   async function getCourse(id) {
     let cls;
@@ -410,26 +407,6 @@ export default function () {
 
   }, [currentUser, course]);
 
-  useEffect(() => {
-    if (!course.participants) {
-      return;
-    }
-
-    if (!course.instructor || !course.instructor.id || !currentUser.id || !currentUser.type) {
-      setParticipantList(course.participants);
-      return;
-    }
-
-    if (course.instructor.id === currentUser.id || currentUser.type === 'admin') {
-      Course.getParticipants(course.id).then(list => {
-        setParticipantList(list);
-      }).catch (err => {
-        log.warn("COURSE INFO:: unable to fetch list of participants");
-      })
-    } else {
-      setParticipantList(course.participants);
-    }
-  }, [course, currentUser])
 
   const goToLogin = async function() {
     history.push(`${path.login}?redirect=${path.courses}/${course.id}`);
@@ -540,21 +517,6 @@ export default function () {
     history.push(path.courses + "/" + course.id + path.joinPath);
   }
 
-  const birthdayHelper = function (user) {
-    if (user.birthday) {
-      // Check range extending to 8 days on either side of today to ensure time differences don't miss a valid birthday
-      const bday = new Date(user.birthday);
-      const start = sub(new Date(course.start_date), {days: 8});
-      const end = add(new Date(course.start_date), {days: 8});
-      bday.setFullYear(start.getFullYear());
-
-      if (isSameDay(bday, new Date(course.start_date)) || isWithinInterval(bday, {start: start, end: end})) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   let paymentProcessingContent = null;
   if (paymentProcessing) {
     paymentProcessingContent = ( 
@@ -609,40 +571,6 @@ export default function () {
         </Grid>
         <Grid container direction="row" justify="flex-start" alignItems="center" alignContent="center" spacing={2}>
           {insta}
-        </Grid>
-      </Card>
-    )
-  }
-
-  let participantsContent = null
-
-  if (participantList && participantList.length) { 
-    let participants = participantList.map(item => (
-      <Grid key={item.username} item xs={6}>
-        <Grid container display='inline' direction='row' alignItems='center'>
-          <Typography variant="body1">{item.username}</Typography>
-          {(currentUser.id === course.instructor.id || currentUser.type === 'admin') && birthdayHelper(item) ? 
-            <BdayIcon bday={item.birthday} /> :
-            null
-          }
-        </Grid>
-      </Grid>
-    ))
-
-    participantsContent = (
-      <Card className={classes.participantContainer}>
-        <Grid container direction="row" justify="flex-start" alignItems="center" alignContent="center" spacing={2}>
-          <Grid item>
-            <People color="primary" />
-          </Grid>
-          <Grid item>
-            <Typography variant="h5">
-              Participants
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container direction="row" justify="flex-start">
-          {participants}
         </Grid>
       </Card>
     )
@@ -788,7 +716,7 @@ export default function () {
             {instructorContent}
           </Grid>
           <Grid item xs={layout.instructorDetailsSize}>
-            {participantsContent}
+            <Participants currentUser={currentUser} course={course} classes={classes} />
           </Grid>
         </Grid>
       </Grid>
