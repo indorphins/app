a44e932aa28ce5999020de40d64503f473eefa7a
afloesch <andrew@vapi.com>
2020-08-15 19:55:41 -0700

    factor pulisher video and controls into separate component

---
+++ src/components/video/publisherControls.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { 
+  Box, 
+  Grid, 
+  IconButton,
+  makeStyles,
+} from '@material-ui/core';
+import { 
+  VideocamOffOutlined, 
+  VideocamOutlined, 
+  MicNone, 
+  MicOffOutlined, 
+} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  publisher: {
+    height: 240,
+    width: 320,
+    background: theme.palette.grey[100],
+  },
+}));
+
+export default function PublisherControls(props) {
+
+  const classes = useStyles();
+  const [publisher, setPublisher] = useState(null);
+  const [publishVideo, setPublishVideo] = useState(true);
+  const [publishAudio, setPublishAudio] = useState(false);
+
+  useEffect(() => {
+    if (props.publisher) {
+      setPublisher(props.publisher);
+    }
+
+    if (props.user && props.course && props.user.id === props.course.instructor.id) {
+      setPublishAudio(true);
+    }
+  }, [props]);
+
+  useEffect(() => {
+    if (publisher) publisher.publishAudio(publishVideo);
+  }, [publishVideo]);
+
+  useEffect(() => {
+    if (publisher) publisher.publishAudio(publishAudio);
+  }, [publishAudio]);
+
+  function toggleAudio() {
+    setPublishAudio(!publishAudio);
+  }
+
+  function toggleVideo() {
+    setPublishVideo(!publishVideo);
+  }
+
+  let videoBtn = (<VideocamOffOutlined />);
+  let videoTitle = "Enable camera";
+  if (publishVideo) {
+    videoBtn = (<VideocamOutlined />);
+    videoTitle = "Disable camera";
+  }
+
+  let micBtn = (<MicOffOutlined />);
+  let micTitle = "Enable microphone";
+  if (publishAudio) {
+    micBtn = (<MicNone />);
+    micTitle = "Disable microphone"
+  }
+
+  let content = (
+    <Box>
+      <Grid id="publisher" className={classes.publisher}></Grid>
+      <IconButton title={videoTitle} onClick={toggleVideo}>
+        {videoBtn}
+      </IconButton>
+      <IconButton title={micTitle} onClick={toggleAudio}>
+        {micBtn}
+      </IconButton>
+    </Box>
+  );
+
+  return content;
+}
