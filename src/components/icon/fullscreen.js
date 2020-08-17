aed7b6351789718ccf5242c3c7c91ed55bc6ce19
afloesch <andrew@vapi.com>
2020-08-16 17:38:47 -0700

    setup basic layout picker

---
+++ src/components/icon/fullscreen.js
@@ -0,0 +1,17 @@
+/* eslint max-len: 0*/
+import React from 'react';
+
+export default function FullscreenIcon(props) {
+
+  let width = "1em";
+  let height = "1em";
+
+  if (props.width) width = props.width;
+  if (props.height) height = props.height;
+
+  return (
+    <svg {...props} width={width} height={height} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z" />
+    </svg>
+  );
+}
---
+++ src/components/video/layout/picker.js
@@ -0,0 +1,76 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { IconButton, Grid, Menu, MenuItem } from '@material-ui/core';
+
+import FullscreenIcon from '../../icon/fullscreen';
+import GridIcon from '../../icon/grid';
+import SplitGridIcon from '../../icon/splitGrid';
+
+export default function LayoutPicker(props) {
+
+  const btn = useRef(null);
+  const [layout, setLayout] = useState("horizontal");
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  useEffect(() => {
+    if (props.layout) setLayout(props.layout);
+  }, [props])
+
+  const handleClick = () => {
+    setAnchorEl(btn.current);
+  };
+
+  const handleClose = function() {
+    setAnchorEl(null);
+  }
+
+  const handleSelect = function(layout) {
+    handleClose();
+    setLayout(layout);
+    if (props.onSelect) props.onSelect(layout);
+  }
+
+  let btnContent = (<FullscreenIcon />)
+
+  if (layout === "grid") {
+    btnContent = (<GridIcon width="20" height="20" />);
+  }
+
+  if (layout === "vertical") {
+    btnContent = (<SplitGridIcon width="20" height="20" style={{ transform: "rotate(90deg)"}} />);
+  }
+  
+  if (layout === "horizontal") {
+    btnContent = (<SplitGridIcon width="20" height="20" />);
+  } 
+  
+  if (layout === "fullscreen") {
+    btnContent = (<FullscreenIcon width="20" height="20" />);
+  }
+
+  return (
+    <Grid>
+      <IconButton ref={btn} onClick={handleClick} title="Video layout">
+        {btnContent}
+      </IconButton>
+      <Menu
+        keepMounted
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+      >
+        <MenuItem title="Fullscreen" onClick={() => { handleSelect("fullscreen"); }}>
+          <FullscreenIcon />
+        </MenuItem>
+        <MenuItem title="Grid" onClick={() => { handleSelect("grid"); }}>
+          <GridIcon />
+        </MenuItem>
+        <MenuItem title="Horizontal" onClick={() => { handleSelect("horizontal"); }}>
+          <SplitGridIcon />
+        </MenuItem>
+        <MenuItem title="Vertical" onClick={() => { handleSelect("vertical"); }}>
+          <SplitGridIcon style={{ transform: "rotate(90deg)"}} />
+        </MenuItem> 
+      </Menu>
+    </Grid>
+  )
+}
