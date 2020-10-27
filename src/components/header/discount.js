def28916ac7acf5d022a80e9452bd4fae76b4690
afloesch <andrew@vapi.com>
2020-10-26 23:42:23 -0700

    add close button

--- src/components/header/discount.js
+++ src/components/header/discount.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, makeStyles, Typography, IconButton } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 
@@ -8,6 +9,11 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
   },
+  close: {
+    position: "absolute",
+    top: 2,
+    right: 6,
+  }
 }));
 
 const getUserSelector = createSelector([state => state.user.data], (user) => {
@@ -28,43 +34,53 @@ export default function Discount() {
   const user = useSelector(state => getUserSelector(state));
   const sessions = useSelector(state => getSessions(state));
   const campaign = useSelector(state => campaignSelector(state));
+  const [content, setContent] = useState(null);
 
-  let content = (<Grid />);
-
-  if (campaign.id && (campaign.discountAmount || campaign.discountRate)) {
-    if (campaign.newUser && (sessions <= 0 && user)) {
-      return content;
-    }
-
-    let text = "";
-    let amount;
-
-    if (campaign.discountAmount) {
-      amount = campaign.discountAmount / 100;
-      text = text + `Book a class now for $${amount} off your next`;
-
-      if (campaign.discountMultiplier > 1) {
-        text = text + ` ${campaign.discountMultiplier} classes`
-      } else {
-        text = text + " class";
-      }
-    }
-
-    content = (
-      <Container className={classes.root}>
-        <Grid container direction="row" justify="center" alignContent="center" alignItems="center" spacing={2}>
-          <Grid item>
-            <img alt="present" src="/img/presentArt.png" style={{maxHeight: 32}} />
-          </Grid>
-          <Grid item>
-            <Typography variant="h4">
-              {text}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    );
+  function close() {
+    setContent(null);
   }
+
+  useEffect(() => {
+    if (campaign.id && (campaign.discountAmount || campaign.discountRate)) {
+      if (campaign.newUser && (sessions <= 0 && user)) {
+        return content;
+      }
+
+      let text = "";
+      let amount;
+
+      if (campaign.discountAmount) {
+        amount = campaign.discountAmount / 100;
+        text = text + `Book a class now for $${amount} off your next`;
+
+        if (campaign.discountMultiplier > 1) {
+          text = text + ` ${campaign.discountMultiplier} classes`
+        } else {
+          text = text + " class";
+        }
+      }
+
+      setContent(
+        <Grid style={{position: "relative"}}>
+          <Container className={classes.root}>
+            <Grid container direction="row" justify="center" alignContent="center" alignItems="center" spacing={2}>
+              <Grid item>
+                <img alt="present" src="/img/presentArt.png" style={{maxHeight: 32}} />
+              </Grid>
+              <Grid item>
+                <Typography variant="h4">
+                  {text}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
+          <IconButton onClick={close} className={classes.close}>
+            <Close />
+          </IconButton>
+        </Grid>
+      );
+    }
+  }, [campaign]);
 
   return content;
 }
