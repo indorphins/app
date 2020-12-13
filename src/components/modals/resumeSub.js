29651b31ef1676611bffa477cd494e3b420deb32
Alex Lindsay <alindsaydev@gmail.com>
2020-12-12 19:35:35 -0800

    resumeSubscription modal

--- src/components/modals/resumeSub.js
+++ src/components/modals/resumeSub.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import { useHistory } from "react-router-dom";
 import { Button, Grid, Modal, Fade, Paper, Typography, makeStyles } from '@material-ui/core';
+import log from '../../log';
+import { create } from '../../api/subscription';
+import { store, actions } from '../../store';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -23,7 +29,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));  
 
-// pull in subscription price and product data + user payment method data
+const getProductsSelector = createSelector([state => state.products], products => {
+  return products;
+});
+
+const paymentDataSelector = createSelector([state => state.user], (data) => {
+  return data.paymentData;
+});
+
+const getDefaultPaymentMethod = createSelector(
+  paymentDataSelector,
+  pd => pd.methods.filter(item => item.default)
+);
 
 /**
  * Modal that will create a subscription without free trial if user chooses to resume subscription
@@ -31,14 +48,122 @@ const useStyles = makeStyles((theme) => ({
  * requires props - openModal, closeModalHandler
  */
 export default function ResumeSubscriptionModal (props) {
+  const [err, setErr] = useState();
+  const [cost, setCost] = useState();
+  const [pMethod, setPMethod] = useState('No Payment Method Added');
   const classes = useStyles();
+  const history = useHistory();
+  const products = useSelector(state => getProductsSelector(state));
+  const defaultPaymentMethod = useSelector(state => getDefaultPaymentMethod(state));
+
+  function isInteger(n) {
+    return n === +n && n === (n|0);
+  }
+
+  useEffect(() => {
+    if (products && products.price && products.price.length > 0) {
+      
+      let c = products.price[0].amount / 100;
+      let costText = "$" + c.toFixed(2);
+
+      if (isInteger(c)) {
+        costText = "$" + c;
+      }
+      
+      setCost(costText)
+    }
+  }, [products]);
+
+  useEffect(() => {
+    if (defaultPaymentMethod && defaultPaymentMethod.length > 0) {
+      setPMethod(`Payment Method: ${defaultPaymentMethod[0].brand.toUpperCase()} ${defaultPaymentMethod[0].last4}`)
+    }
+  }, [defaultPaymentMethod])
 
   const swapPaymentHandler = () => {
-    // console.log("Swap p method!");
+    setErr(null);
+    history.push('/profile');
   }
 
   const resumeSubHandler = () => {
-    // console.log("REsume sub!");
+    if (!defaultPaymentMethod) {
+      setErr("Add a payment method to subscribe")
+      return
+    }
+
+    if (!products || !products.product || !products.price || products.price.length === 0) {
+      log.warn("No products in redux store");
+      setErr("No subscription data available")
+      return;
+    }
+
+    create(products.product.id, products.price[0].id)
+      .then(sub => {
+        log.info("SUBSCRIPTION:: created ", sub);
+        store.dispatch(actions.user.setSubscription(sub));
+        setErr(null);
+        props.closeModalHandler();
+      }).catch(err => {
+        log.warn("SUBSCRIPTION:: error creating ", err);
+        setErr(err.message);
+      });
+  }
+
+  let content;
+
+  if (!products || !products.product || !products.price || products.price.length === 0) {
+    content = (
+      <Paper className={classes.modalContent}>
+        <Grid container id='modal-description' justify='center'>
+          <Typography variant="h3">Subscription Data Unavailable</Typography>
+        </Grid> 
+        <br />
+        <Grid container id='modal-description2' justify='center'>
+          <Typography variant="body2">Try again later</Typography>  
+        </Grid>
+        <br />
+        <Grid container id='modal-buttons' justify='center'>
+          <Button onClick={props.closeModalHandler} variant="contained" color="primary" className={classes.modalBtn}>
+            Go Back
+          </Button>
+        </Grid>
+      </Paper>
+    )
+  } else {
+    content = (
+      <Paper className={classes.modalContent}>
+        <Grid container id='modal-description' justify='center'>
+          <Typography variant="h3">Unlimited Classes for just {cost}</Typography>
+          <br />
+          <br />
+          <Typography variant="body2">
+            OFFER TERMS: take as many classes as you’d like across our platform for just {cost}/mo. 
+            You’ll be billed automatically each month. Terms & Services apply across all classes.
+          </Typography>
+        </Grid>
+        <br />
+        <Typography variant="body2">{pMethod}</Typography>
+        <br />
+        <Grid container id='modal-buttons' justify='center'>
+          <Button
+            onClick={swapPaymentHandler}
+            variant="contained"
+            className={classes.modalBtn}
+          >
+            Swap Payment Method
+          </Button>
+          <Button onClick={resumeSubHandler} variant="contained" color="primary" className={classes.modalBtn}>
+            Resume Subscription
+          </Button>
+        </Grid>
+        <br />
+        <Grid container id='error-container' justify='center'>
+          {err ? 
+            <Typography variant="body2">{err}</Typography> 
+          : null}
+        </Grid>
+      </Paper>
+    )
   }
 
   return (
@@ -50,32 +175,7 @@ export default function ResumeSubscriptionModal (props) {
       aria-describedby="modal-description"
     >
       <Fade in={props.openModal}>
-        <Paper className={classes.modalContent}>
-          <Grid container id='modal-description' justify='center'>
-            <Typography variant="h3">Unlimited Classes for just $XX.XX/mo!</Typography>
-            <br />
-            <br />
-            <Typography variant="body2">
-              OFFER TERMS: take as many classes as you’d like across our platform for just $49.99/mo. 
-              You’ll be billed automatically each month. Terms & Services apply across all classes.
-            </Typography>
-          </Grid>
-          <br />
-          <Typography variant="body2">Payment method: VISA XXX2PRO</Typography>
-          <br />
-          <Grid container id='modal-buttons' justify='center'>
-            <Button
-              onClick={swapPaymentHandler}
-              variant="contained"
-              className={classes.modalBtn}
-            >
-              Swap Payment Method
-            </Button>
-            <Button onClick={resumeSubHandler} variant="contained" color="primary" className={classes.modalBtn}>
-              Resume Subscription
-            </Button>
-          </Grid>
-        </Paper>
+        {content}
       </Fade>
     </Modal>
   )
