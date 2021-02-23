67cc69e1d0b8ce149c2095d0c54c5e7f65391b71
Alex Lindsay <alindsaydev@gmail.com>
2021-02-22 19:35:05 -0800

    mobile beta page pre device picker

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1138,6 +1138,7 @@ export default function Video(props) {
           initPublisher={initPublisher}
           onJoined={handleJoin}
           noDevices={noDevices}
+          isMobile={isMobile()}
         />
       </Grid>
     );
